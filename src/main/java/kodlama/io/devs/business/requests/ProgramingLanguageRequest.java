f0914b0c0545e57f3aeaee025958ca26233fc3c5
akifz21 <akifz2199@gmail.com>
2022-11-03 10:46:38 +0300

    spring CRUD operations and psql connection

---
+++ src/main/java/kodlama/io/devs/business/requests/ProgramingLanguageRequest.java
@@ -0,0 +1,12 @@
+package kodlama.io.devs.business.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProgramingLanguageRequest {
+    private  String name;
+}
---
+++ src/main/java/kodlama/io/devs/business/responses/ProgramingLanguageResponse.java
@@ -0,0 +1,13 @@
+package kodlama.io.devs.business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProgramingLanguageResponse {
+    private  int id;
+    private String name;
+}
